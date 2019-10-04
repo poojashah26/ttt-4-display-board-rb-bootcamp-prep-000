2ca112f9c9b22fd29bc98ec3bd03455e808312be
Pooja Shah <pooja_shah26@hotmail.co.uk>
2019-10-04 10:54:28 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,6 +2,7 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
+<<<<<<< HEAD
 def display_board
   puts "   |   |   "
   puts "-----------"
@@ -20,3 +21,8 @@ end
 
 board[4] = "X"
 display_board(board)
+=======
+def display_board(board)
+  puts "#{board[0]}", "#{board[1]}", "#{board[2]}", "#{board[3]}", "#{board[4]}", "#{board[5]}", "#{board[6]}", "#{board[7]}", "#{board[8]}"
+end
+>>>>>>> e9910782773ab6424aa28d7086a4d6205352ec5e
